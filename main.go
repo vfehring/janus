32a812ccac0c29c3112eef00e37524b864f5abb8
Vincenzo Fehring <vincenzo.fehring@me.com>
2023-05-25 03:08:33 -0500

    Moved MessageCreate to an external event file.

--- main.go
+++ main.go
@@ -48,7 +48,7 @@ func main() {
 
 	// Register the Discord events
 	dg.AddHandler(events.Ready)
-	dg.AddHandler(messageCreate)
+	dg.AddHandler(events.MessageCreate)
 
 	// In this example, we only care about receiving message events.
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
@@ -68,24 +68,4 @@ func main() {
 
 	// Cleanly close down the Discord session.
 	dg.Close()
-}
-
-// This function will be called (due to AddHandler above) every time a new
-// message is created on any channel that the authenticated bot has access to.
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	// Ignore all messages created by the bott itself
-	// This isn't required in this specific example, but it's a good practice.
-	if m.Author.ID == s.State.User.ID {
-		return
-	}
-	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
-	}
-
-	// If the message is "pong" reply with "Ping!"
-	if m.Content == "pong" {
-		s.ChannelMessageSend(m.ChannelID, "Ping!")
-	}
 }
